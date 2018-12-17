8c9a70f5abd1108cf15b3a0f0b8874bb98d91fc7
tbajorek <tbajorek3@gmail.com>
2018-12-17 02:42:32 +0100

    Small cleaning

--- src/index.js
+++ src/index.js
@@ -65,21 +65,6 @@ server.on('user.disconnected', (e) => {
         console.log('user '+e.data.user.id+' is offline');
     });
 });
-/*server.on('connection.opened', (e) => {
-    console.log('connection.opened');
-});
-server.on('connection.closed', (e) => {
-    console.log('connection.closed');
-});
-server.on('session.created', (e) => {
-    console.log('session.created');
-    //console.log(e.data.session);
-});
-server.on('message.received', (e) => {
-    console.log('message.received', e.data.message);
-});
-server.on('user.communication', (e) => {
-    console.log('user.communication', e.data.message);
-});*/
+
 
 server.start();
